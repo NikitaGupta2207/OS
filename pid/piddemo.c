6289d2963e1850f7149f2e67e2550f75af9e4047
Nikita gupta <nikitagupta2509@gmail.com>
2020-10-14 22:45:46 +0530

    pid and ppid demo

---
+++ pid/piddemo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+int main(int agrc, char const *argv[])
+{
+    pid_t id;
+    pid_t parent_id; 
+
+    id = getpid();
+    parent_id=getppid();
+    printf("PID: %d\n", id);
+    printf("PPID: %d\n", parent_id);
+    return 0;
+}
